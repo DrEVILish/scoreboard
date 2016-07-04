fad3e4071c134703800a11408ec27b684e0a9a1d
Ryan Freeman <drevilish@me.com>
2016-07-04 16:58:02 +0100

    Fixed npm install issue

--- init/30_update.sh
+++ init/30_update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 [[ ! -d /usr/src/app/scoreboard/.git ]] && git clone \
-https://github.com/DrEVILish/scoreboard /usr/src/app/scoreboard
-
+https://github.com/DrEVILish/scoreboard /usr/src/app/scoreboard 
+cd /usr/src/app/scoreboard 
+npm install
 chown -R abc:abc /usr/src/app/scoreboard
 
 # opt out for autoupdates
@@ -10,4 +11,5 @@ chown -R abc:abc /usr/src/app/scoreboard
 
 cd /usr/src/app/scoreboard
 git pull
+npm install
 chown -R abc:abc /usr/src/app/scoreboard
